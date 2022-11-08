af6a883034f2908e4ab3a5a7f64531c2cf1c7f84
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-11-08 16:11:04 -0300

    feat: rout creatuser

--- src/comtrollers/userController.js
+++ src/comtrollers/userController.js
@@ -3,6 +3,7 @@ import * as repositors from '../repositoris/repositors.js'
 async function insertUser ( req, res ){
     const {name} = req.body;
     try {
+        
         await repositors.insert({ table: "users(name)" , iten:[name] })        
 
         res.sendStatus(200)
