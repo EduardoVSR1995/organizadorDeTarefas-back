b6b5effabf38180272766750c17da7aaa9321b32
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-11-08 19:34:49 -0300

    fea: inplantation TS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, {json} from "express";
-import cors from "cors";
-import dotenv from 'dotenv';
+import cors from 'cors';
+import dotenv from "dotenv";
 import comanders from './comanders.js'
 
 dotenv.config();
