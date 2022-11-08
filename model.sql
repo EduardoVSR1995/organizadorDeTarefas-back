f0f08d3540b770475988ff9a3f1e478241b86cd6
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-11-08 12:28:54 -0300

    build: database model

--- model.sql
+++ model.sql
@@ -1,19 +1,21 @@
+CREATE DATABASE "organizador" ;
+
 CREATE TABLE "usuario"(
     "id"            SERIAL PRIMARY KEY,
     "name"          TEXT NOT NULL
-)
+);
 
 CREATE TABLE "tarefa"(
     "id"            SERIAL PRIMARY KEY,
     "tarefa"        TEXT NOT NULL,
     "diaFim"        DATE NOT NULL,
     "diaCriado"     DATE DEFAULT NOW() NOT NULL,
-    "responsavel"   INTEGER REFERENCES "responsavel"("id") NOT NULL,
+    "responsavel"   INTEGER REFERENCES "usuario"("id") NOT NULL,
     "status"        BOOLEAN DEFAULT TRUE NOT NULL
- )
+ );
 
  CREATE TABLE "responsavel"(
     "id"            SERIAL PRIMARY KEY,
     "usuarioId"     INTEGER REFERENCES "usuario"("id") NOT NULL,
     "tarefaId"     INTEGER REFERENCES "tarefa"("id") NOT NULL
- )
+ );
